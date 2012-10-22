eb529a4dae26f1a0c9519129b7ae5eb618684bf4
Leonardo Mosquera <ldmosquera@gmail.com>
2012-10-22 01:52:23 -0300

    Linux related aliases.

---
+++ .profile.d/linux.bash
@@ -0,0 +1,8 @@
+alias pst='pstree -pu $USER'
+alias atop='atop 1 -f'
+
+alias ndg='nautilus --no-desktop'
+alias go='xdg-open'
+
+#roughly sorts by bind interface + port
+alias ns='netstat -ntapo | sort -n'
